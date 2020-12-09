79d7352c8a837fecb689df3687a48b284d72cfc9
Garrett Cintron <grrttcintron@gmail.com>
2020-12-08 16:44:35 -0800

    code added

---
+++ server.js
@@ -0,0 +1,9 @@
+'use strict';
+
+//libraries
+require('dotenv').config();
+const express = require('express'); //server
+const app = express(); //assigning the server to a variable
+const PORT = process.env.PORT || 3000
+
+app.get('/person',)
